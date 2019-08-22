db2993325b2a58323b4f163cebd6ffc73074afb5
kmilo9713 <kmilo9713.cl@gmail.com>
2019-08-22 16:09:01 -0500

    Actualizacion codigo busquedas

--- Subject-Unimag/Seguimiento1/tree-searchs.py
+++ Subject-Unimag/Seguimiento1/tree-searchs.py
@@ -11,6 +11,9 @@
 
         Topic: Tree Searchs
         GitHub: https://github.com/kmilo9713/
+
+        Algorithms bfs and dfs taken from: https://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
+        # a sample graph
 """
 
 try:
@@ -21,13 +24,9 @@ except ImportError:
 
 class tree_searchs():
 
-    def __init__(self, name, grafo):
+    def __init__(self, name):
         'Constructer of the class'
         self.__name = name
-        self.__grafo = grafo
-        self.__visitados = []   #Lista de visitados
-        self.__pila = []    #Para recorrido en anchura
-        self.__cola = []    #Para recorrido en profundidad
 
     @property
     def name(self):
@@ -44,115 +43,33 @@ class tree_searchs():
         'This method resets the name for the tree'
         self.__name = None
 
-    @property
-    def grafo(self):
-        'This method returns the trees\'s graph'
-        return self.__grafo
+    def bfs_paths(self, graph, start, goal):
+        queue = [(start, [start])]
+        while queue:
+            (vertex, path) = queue.pop(0)
+            print path
+            l = list(set(graph[vertex]) - set(path))
+            for next in sorted(l):
+                print graph[vertex]
+                if next == goal:
+                    yield path + [next]
+                else:
+                    queue.append((next, path + [next]))
     
-    @grafo.setter
-    def grafo(self, grafo):
-        'This method changes the trees\'s graph'
-        self.__grafo = grafo
-    
-    @grafo.deleter
-    def grafo(self):
-        'This method resets the trees\'s graph'
-        self.__grafo = None
-    
-    def anchura(self, start, end=None):
-        'This method makes a breadth search in the graph'
+    def dfs_paths(self, graph, start, goal):
+        stack = [(start, [start])]
+        while stack:
+            (vertex, path) = stack.pop()
+            print 'path: ' , path
+            l = list(set(graph[vertex]) - set(path))
+            for next in sorted(l, reverse=True):
+                            
+                if next == goal:
+                    yield path + [next]
+                else:
+                    stack.append((next, path + [next]))
 
-        print("+ [INFO] Busqueda en anchura -> COMIENZO")
-
-        if end in self.__grafo:
-            print("+ [INFO] Buscando objetivo ", end)
-        elif end:
-            print("+ [INFO] Objetivo no se encuentra en el grafo")
-
-        if start not in self.__grafo:
-            print("No existe ese nodo en el grafo!")
-            return
-        
-        self.__visitados = []
-        self.__cola = []
-
-        #print("Ingreso origen a cola: ", start)
-        self.__cola.insert(0, start)
-
-        while len(self.__cola) != 0:    #podria tambien while self.__cola
-            
-            actual = self.__cola.pop()
-            
-            if actual not in self.__visitados:
-                self.__visitados.extend(actual)
-
-            if actual == end:
-                print("+ [INFO] Objetivo encontrado")
-                break;
-            
-            try:
-                for key, _ in self.__grafo[actual]:
-                    if key not in self.__visitados:
-                        self.__cola.insert(0, key)
-            
-            except KeyError:
-                print("+ [WARNING] Nodo ", actual, " no tiene nodos adyacentes")
-
-        print("+ [INFO] Busqueda en anchura -> TERMINADO")
-        print("+ [INFO] Resultado ANCHURA: ", self.__visitados, "\n")
-    
-    def profundidad(self, start, end=None):
-        'This method makes a depth search in the graph'
-
-        print("+ [INFO] Busqueda en profundidad -> COMIENZO")
-
-        if end in self.__grafo:
-            print("+ [INFO] Buscando objetivo ", end)
-        elif end:
-            print("+ [INFO] Objetivo no se encuentra en el grafo")
-
-        if start not in self.__grafo:
-            print("No existe ese nodo en el grafo!")
-            return
-        
-        self.__visitados = []
-        self.__pila = []
-
-        #print("Ingreso origen a pila: ", start)
-        self.__pila.append(start)
-
-        while len(self.__pila) != 0:    #podria tambien while self.__pila
-            
-            actual = self.__pila.pop()
-
-            #print("Saco nodo de pila: ", actual)
-            #print("Pila: ", self.__pila)
-
-            if actual not in self.__visitados:
-                self.__visitados.extend(actual)
-                #print("Agrego a lista de visitados: ", actual)
-                #print("Lista de visitados: ", self.__visitados)
-
-            if actual == end:
-                print("+ [INFO] Objetivo encontrado")
-                break;
-
-            try:
-
-                for key, _ in self.__grafo[actual]:
-                    if key not in self.__visitados:
-                        self.__pila.append(key)
-                        #print("1. Ingreso en pila: ", key)
-                        #print("2. Lista de visitados: ", self.__visitados)
-                        #print("3. Pila: ", self.__pila)
-
-            except KeyError:
-                print("+ [WARNING] Nodo ", actual, " no tiene nodos adyacentes")
-
-        print("+ [INFO] Busqueda en profundidad -> TERMINADO")
-        print("+ [INFO] Resultado PROFUNDIDAD: ", self.__visitados, "\n")
-    
-    def anchura_costo_uniforme(self, start, end=None):
+    def anchura_costo_uniforme(self, grafo, start, end=None):
         'This method makes a breadth search with uniform cost in the graph'
         #REVISAR CON LA PROFESORA
         
@@ -160,31 +77,31 @@ class tree_searchs():
 
         print("+ [INFO] Busqueda de costo uniforme -> COMIENZO")
 
-        if end in self.__grafo:
+        if end in grafo:
             print("+ [INFO] Buscando objetivo ", end)
         elif end:
             print("+ [INFO] Objetivo no se encuentra en el grafo")
 
-        if start not in self.__grafo:
+        if start not in grafo:
             print("No existe ese nodo en el grafo!")
             return
         
-        self.__visitados = []
-        self.__cola = queue.PriorityQueue(len(self.__grafo))    #Creo cola de prioridad
+        visitados = []
+        cola = queue.PriorityQueue(len(grafo))    #Creo cola de prioridad
 
         #print("Ingreso origen a cola: ", start)
-        self.__cola.put((0, start))
+        cola.put((0, start))
 
-        while self.__cola.qsize() != 0:    #podria tambien while self.__cola ó !self.__cola.Empty()
+        while cola.qsize() != 0:    #podria tambien while self.__cola ó !self.__cola.Empty()
             
             #print("Cola: ", self.__cola.qsize())
             #print("Visitados: ", self.__visitados)
 
-            actual = self.__cola.get()  #Sacamos el de menor costo
+            actual = cola.get()  #Sacamos el de menor costo
             print("+ [INFO] Saco de la cola: ", actual)
 
-            if actual[1] not in self.__visitados:
-                self.__visitados.extend(actual[1])
+            if actual[1] not in visitados:
+                visitados.extend(actual[1])
                 print("+ [INFO] Añado nodo a visitados: ", actual[1])
 
             if actual[1] == end:
@@ -192,19 +109,71 @@ class tree_searchs():
                 break
             
             try:
-                for key, value in self.__grafo[actual[1]]:
-                    if key not in self.__visitados:
+                for key, value in grafo[actual[1]]:
+                    if key not in visitados:
                         #print("+ [INFO] Añado nodo a cola: ", key)
-                        self.__cola.put((value, key)) #Introducimos con la cola organizada descendentemente
+                        cola.put((value, key)) #Introducimos con la cola organizada descendentemente
             
             except KeyError:
                 print("+ [WARNING] Nodo ", actual[1], " no tiene nodos adyacentes")
 
         print("+ [INFO] Busqueda de costo uniforme -> TERMINADO")
-        print("+ [INFO] Resultado COSTO UNIFORME: ", self.__visitados, "\n")
+        print("+ [INFO] Resultado COSTO UNIFORME: ", visitados, "\n")
 
-        def profundidad_iterativa(self):
-            pass
+grafo_jarra = {
+
+    '(0,0)': ['(4,0)', '(0,3)'],
+    '(4,0)': ['(4,3)', '(0,0)', '(1,3)'],
+    '(4,3)': ['(0,3)', '(4,0)'],
+    '(0,3)': ['(4,3)', '(0,0)', '(3,0)'],
+    '(1,3)': ['(4,3)', '(0,3)', '(1,0)', '(4,0)'],
+    '(1,0)': ['(4,0)', '(1,3)', '(0,0)', '(0,1)'],
+    '(0,1)': ['(4,1)', '(0,3)', '(0,0)', '(1,0)'],
+    '(4,1)': ['(4,3)', '(0,1)', '(4,0)', '(3,3)'],
+    '(3,0)': ['(3,3)', '(4,0)', '(0,0)', '(0,3)'],
+    '(3,3)': ['(4,3)', '(0,3)', '(3,0)', '(4,2)'],
+    '(4,2)': ['(4,3)', '(0,2)', '(4,0)', '(3,3)'],
+    '(0,2)': ['(4,2)', '(0,3)', '(0,0)', '(2,0)'],
+    '(2,0)': ['(2,3)', '(4,0)', '(0,0)', '(0,2)'],
+    '(2,3)': ['(4,3)', '(0,3)', '(2,0)', '(4,1)'],
+}
+
+grafo_granjero = {
+    '(0,0,0,0)': ['(1,1,0,0)'],
+    '(1,1,0,0)': ['(0,0,0,0)', '(0,1,0,0)'],
+    '(0,1,0,0)': ['(1,1,0,0)', '(1,1,0,1)', '(1,1,1,0)'],
+    '(1,1,0,1)': ['(0,0,0,1)', '(0,1,0,0)'],
+    '(0,0,0,1)': ['(1,1,0,1)', '(1,0,1,1)'],
+    '(1,1,1,0)': ['(0,1,0,0)', '(0,0,1,0)'],
+    '(0,0,1,0)': ['(1,1,1,0)', '(1,0,1,1)'],
+    '(1,0,1,1)': ['(0,0,0,1)', '(0,0,1,0)', '(0,0,1,1)'],
+    '(0,0,1,1)': ['(1,0,1,1)', '(1,1,1,1)'],
+    '(1,1,1,1)': ['(0,0,1,1)'],
+}
+
+graph3 = {
+    # Origen : destinos = (destino, peso_arista)
+    "S":[("A", 1), ("B", 5), ("C", 15)],
+    "A":[("G", 10), ("S", 1)],
+    "G":[("C", 5), ("A", 10)],
+    "C":[("S", 15), ("G", 5)],
+    "B":[("G", 5), ("S", 5)],
+}
+
+tree1 = tree_searchs("Arbol 1")
+
+print next(tree1.dfs_paths(grafo_jarra, '(0,0)', '(2,0)')) 
+print next(tree1.bfs_paths(grafo_jarra, '(0,0)', '(2,0)'))
+tree1.anchura_costo_uniforme(graph3, "S", "B")
+
+"""
+
+graph = {'A': [('B', 1), ('C', 1)],
+         'B': [('A', 1), ('D',1), ('E', 1)],
+         'C': [('A', 1), ('F', 1)],
+         'D': [('B', 1)],
+         'E': [('B', 1), ('F', 1)],
+         'F': [('C', 1), ('E', 1)]}
 
 graph1 = {
     "a":[("b", 1), ("c", 1), ("d", 1)], # Origen : destinos = (destino, peso_arista)
@@ -258,9 +227,89 @@ graph4 = {
 
 #https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fadvanceintelligence.files.wordpress.com%2F2015%2F02%2Fdat.png&f=1
 
-tree1 = tree_searchs("Arbol 1", graph2)
-tree1.profundidad("D", "Z")
-tree1.anchura("D", "Z")
+graph_class = {
+         'A': [('B', 1), ('C', 1), ('D', 1)],
+         'B': [('A', 1), ('E', 1), ('F', 1)],
+         'C': [('A', 1), ('G', 1), ('H', 1)],
+         'D': [('A', 1), ('G', 1), ('H', 1)],
+         'E': [('B', 1), ('I', 1)],
+         'F': [('B', 1), ('I', 1), ('K', 1)],
+         'G': [('C', 1), ('D', 1), ('K', 1)],
+         'H': [('C', 1), ('D', 1), ('L', 1)],
+         'I': [('E', 1),('F', 1), ('J', 1), ('L', 1)],
+         'J': [('I', 1), ('K', 1), ('L', 1)],
+         'K': [('F', 1), ('G', 1), ('J', 1), ('L', 1)],
+         'L': [('H', 1), ('I', 1), ('K', 1)],
+         }
 
-tree1.grafo = graph4
-tree1.anchura_costo_uniforme("S", "G")
+grafo_jarra = {
+
+    '(0,0)': [('(4,0)', 1), ('(0,3)', 1)],
+    '(4,0)': [('(4,3)', 1), ('(0,0)', 1), ('(1,3)', 1)],
+    '(4,3)': [('(0,3)', 1), ('(4,0)', 1)],
+    '(0,3)': [('(4,3)', 1), ('(0,0)', 1), ('(3,0)', 1)],
+    '(1,3)': [('(4,3)', 1), ('(0,3)', 1), ('(1,0)', 1), ('(4,0)', 1)],
+    '(1,0)': [('(4,0)', 1), ('(1,3)', 1), ('(0,0)', 1), ('(0,1)', 1)],
+    '(0,1)': [('(4,1)', 1), ('(0,3)', 1), ('(0,0)', 1), ('(1,0)', 1)],
+    '(4,1)': [('(4,3)', 1), ('(0,1)', 1), ('(4,0)', 1), ('(3,3)', 1)],
+    '(3,0)': [('(3,3)', 1), ('(4,0)', 1), ('(0,0)', 1), ('(0,3)', 1)],
+    '(3,3)': [('(4,3)', 1), ('(0,3)', 1), ('(3,0)', 1), ('(4,2)', 1)],
+    '(4,2)': [('(4,3)', 1), ('(0,2)', 1), ('(4,0)', 1), ('(3,3)', 1)],
+    '(0,2)': [('(4,2)', 1), ('(0,3)', 1), ('(0,0)', 1), ('(2,0)', 1)],
+    '(2,0)': [('(2,3)', 1), ('(4,0)', 1), ('(0,0)', 1), ('(0,2)', 1)],
+    '(2,3)': [('(4,3)', 1), ('(0,3)', 1), ('(2,0)', 1), ('(4,1)', 1)],
+}
+
+def anchura_costo_uniforme(self, start, end=None):
+        'This method makes a breadth search with uniform cost in the graph'
+        #REVISAR CON LA PROFESORA
+        
+        path = []
+
+        print("+ [INFO] Busqueda de costo uniforme -> COMIENZO")
+
+        if end in self.__grafo:
+            print("+ [INFO] Buscando objetivo ", end)
+        elif end:
+            print("+ [INFO] Objetivo no se encuentra en el grafo")
+
+        if start not in self.__grafo:
+            print("No existe ese nodo en el grafo!")
+            return
+        
+        self.__visitados = []
+        self.__cola = queue.PriorityQueue(len(self.__grafo))    #Creo cola de prioridad
+
+        #print("Ingreso origen a cola: ", start)
+        self.__cola.put((0, start))
+
+        while self.__cola.qsize() != 0:    #podria tambien while self.__cola ó !self.__cola.Empty()
+            
+            #print("Cola: ", self.__cola.qsize())
+            #print("Visitados: ", self.__visitados)
+
+            actual = self.__cola.get()  #Sacamos el de menor costo
+            print("+ [INFO] Saco de la cola: ", actual)
+
+            if actual[1] not in self.__visitados:
+                self.__visitados.extend(actual[1])
+                print("+ [INFO] Añado nodo a visitados: ", actual[1])
+
+            if actual[1] == end:
+                print("+ [INFO] Objetivo encontrado")
+                break
+            
+            try:
+                for key, value in self.__grafo[actual[1]]:
+                    if key not in self.__visitados:
+                        #print("+ [INFO] Añado nodo a cola: ", key)
+                        self.__cola.put((value, key)) #Introducimos con la cola organizada descendentemente
+            
+            except KeyError:
+                print("+ [WARNING] Nodo ", actual[1], " no tiene nodos adyacentes")
+
+        print("+ [INFO] Busqueda de costo uniforme -> TERMINADO")
+        print("+ [INFO] Resultado COSTO UNIFORME: ", self.__visitados, "\n")
+
+
+"""
