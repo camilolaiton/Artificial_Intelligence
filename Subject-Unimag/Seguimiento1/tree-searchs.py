88425fcabd6db9263d4fd76a05a3a9359c41dbf9
kmilo9713 <kmilo9713.cl@gmail.com>
2019-08-31 22:46:36 -0500

    algoritmos modificados para misma clase de grafo y profundidad iterativa creado

--- Subject-Unimag/Seguimiento1/tree-searchs.py
+++ Subject-Unimag/Seguimiento1/tree-searchs.py
@@ -48,15 +48,14 @@ class tree_searchs():
         while queue:
             #print("Cola antes: ", queue, "\n")
             (vertex, path) = queue.pop(0)
-            print("Vertex: ", vertex)
+            #print("Vertex: ", vertex)
             #print("Cola despues: ", queue, "\n")
 
-            print("Camino: ", path)
+            #print("Camino: ", path)
 
             l = list(set(graph[vertex]) - set(path))    #Quedan nodos a los que puedo viajar
 
             for next in sorted(l):
-                print("Camino vertex: ", graph[vertex])
                 if next == goal:
                     yield path + [next]
                 else:
@@ -65,8 +64,6 @@ class tree_searchs():
     def dfs_paths(self, graph, start, goal):
         
         stack = [(start, [start])]
-        
-        nivel = 0
 
         while stack:
             (vertex, path) = stack.pop()
@@ -78,40 +75,75 @@ class tree_searchs():
             for next in sorted(l, reverse=True):
                             
                 if next == goal:
-                    yield (path + [next], True)
+                    yield path + [next]
                 else:
                     stack.append((next, path + [next]))
 
-    def dfs_paths_limit(self, graph, start, goal, outer_limit):
+    def __dfs_paths_b(self, graph, start, goal, depth_limit, outer_limit):    #Parte b primero en profundidad - madified by Camilo Laiton
 
-    	x = 0
+        stack = [ (start, [start], 0) ] #Inico
+        camino_niveles = [] #Array para guardar el recorrido por niveles cuando no se encuentra solucion
+
+        while stack:
+            (actual, camino, nivel_nodo) = stack.pop()
+
+            if actual not in camino_niveles:
+                camino_niveles.append(actual)   #Para guardar el recorrido por niveles [No es necesario para funcionamiento]
+            #print("Saco de pila: ", actual, " llevo camino: ", camino, " nivel: ", nivel_nodo)
+            
+            l = None
+
+            if nivel_nodo+1 <= depth_limit: #Para saber si necesito buscar en el otro nivel
+                l = list( set(graph[actual]) - set(camino) )
+
+            if l:
+                for next in sorted(l, reverse=True):
+
+                    if next == goal:
+                        return (camino + [next], 1)
+
+                    else:
+                        if nivel_nodo+1 <= depth_limit:
+                            #print("Entro hijos de ", actual, " nivel ", nivel_nodo+1)
+                            stack.append((next, camino + [next], nivel_nodo+1)) #meto nodos del nivel y aumento el nivel en 1
+
+        if depth_limit == outer_limit:  #salgo por limite general
+            return (camino, -1)
+
+        return (camino_niveles, 0)  #Retorno que no encontre solucion pero puedo seguir iterando
+
+    def dfs_paths_limit(self, graph, start, goal, limite_general):  #Parte A primero en profundidad - modified by Camilo Laiton
+
+    	depth_limit = 0
 
     	while(True):
-    		val = next(self.dfs_paths(graph, start, goal, x))
-    		print("\n[+] Cantidad iterada: ", x, "\n")
-    		print("[+] Camino: ", val[0])
+            result = self.__dfs_paths_b(graph, start, goal, depth_limit, limite_general)
 
-    		if(val[1]):
-    			print("[+] Saliendo del limite")
-    			break
+            print("\n[+] Cantidad iterada: ", depth_limit)
 
-    		if(outer_limit > x):
-    			x = x + 1
-    		else:
-    			break
+            if result[1] == -1: #Solution wasn't found
+                print("La solucion no fue encontrada, nivel total: ", depth_limit)
+                break
+            
+            elif result[1] == 1:
+                print("Solucion encontrada en el nivel: ", depth_limit)
+                print("Solucion: ", result[0])
+                break
+            
+            else:
+                print("Solucion no encontrada aún en nivel ", depth_limit, "aumentamos el nivel maximo")
+                print("Camino recorrido: ", result[0])
 
-    def implementacion_costo_uniforme(self, origen, destino):
-        graph = self.__readGraph()
-        self.logica_costo_uniforme(graph, origen, destino)
+            depth_limit = depth_limit + 1
 
-    def logica_costo_uniforme(self, graph, start, end):
+    def costo_uniforme(self, graph, start, end):
 
         if start not in graph:
-            raise TypeError(str(start) + ' not found in graph !')
+            raise TypeError(str(start) + ' no fue encontrado en el grafo!')
             return
 
         if end not in graph:
-            raise TypeError(str(end) + ' not found in graph !')
+            raise TypeError(str(end) + ' no fue encontrado en el grafo!')
             return
         
         queue_a = queue.PriorityQueue()
@@ -134,107 +166,96 @@ class tree_searchs():
                 temp.append(neighbor)
                 queue_a.put((cost + graph[current][neighbor], temp))
         
-    def __readGraph(self):
-        graph = {}
-        
-        graph['Arad'] = {}
-        graph['Arad']['Zerind'] = 75
-        graph['Arad']['Timisoara'] = 118
-        graph['Arad']['Sibiu'] = 140
+def crear_grafo_rumania():
+    graph = {}
+    
+    graph['Arad'] = {}
+    graph['Arad']['Zerind'] = 75
+    graph['Arad']['Timisoara'] = 118
+    graph['Arad']['Sibiu'] = 140
 
-        graph['Zerind'] = {}
-        graph['Zerind']['Oradea'] = 71
-        graph['Zerind']['Arad'] = 75
+    graph['Zerind'] = {}
+    graph['Zerind']['Oradea'] = 71
+    graph['Zerind']['Arad'] = 75
 
-        graph['Timisoara'] = {}
-        graph['Timisoara']['Lugoj'] = 111
-        graph['Timisoara']['Arad'] = 118
+    graph['Timisoara'] = {}
+    graph['Timisoara']['Lugoj'] = 111
+    graph['Timisoara']['Arad'] = 118
 
-        graph['Sibiu'] = {}
-        graph['Sibiu']['Arad'] = 140
-        graph['Sibiu']['Oradea'] = 151
-        graph['Sibiu']['Fagaras'] = 99
-        graph['Sibiu']['RimnicuVilcea'] = 80
+    graph['Sibiu'] = {}
+    graph['Sibiu']['Arad'] = 140
+    graph['Sibiu']['Oradea'] = 151
+    graph['Sibiu']['Fagaras'] = 99
+    graph['Sibiu']['RimnicuVilcea'] = 80
 
-        graph['Oradea'] = {}    
-        graph['Oradea']['Zerind'] = 71
-        graph['Oradea']['Sibiu'] = 151
+    graph['Oradea'] = {}    
+    graph['Oradea']['Zerind'] = 71
+    graph['Oradea']['Sibiu'] = 151
 
-        graph['Lugoj'] = {}    
-        graph['Lugoj']['Timisoara'] = 111
-        graph['Lugoj']['Mehadia'] = 70
+    graph['Lugoj'] = {}    
+    graph['Lugoj']['Timisoara'] = 111
+    graph['Lugoj']['Mehadia'] = 70
 
-        graph['RimnicuVilcea'] = {}    
-        graph['RimnicuVilcea']['Sibiu'] = 80
-        graph['RimnicuVilcea']['Pitesti'] = 97
-        graph['RimnicuVilcea']['Craiova'] = 146
+    graph['RimnicuVilcea'] = {}    
+    graph['RimnicuVilcea']['Sibiu'] = 80
+    graph['RimnicuVilcea']['Pitesti'] = 97
+    graph['RimnicuVilcea']['Craiova'] = 146
 
-        graph['Mehadia'] = {} 
-        graph['Mehadia']['Lugoj'] = 70
-        graph['Mehadia']['Dobreta'] = 75
+    graph['Mehadia'] = {} 
+    graph['Mehadia']['Lugoj'] = 70
+    graph['Mehadia']['Dobreta'] = 75
 
-        graph['Craiova'] = {} 
-        graph['Craiova']['Dobreta'] = 120
-        graph['Craiova']['Pitesti'] = 138
-        graph['Craiova']['RimnicuVilcea'] = 146
+    graph['Craiova'] = {} 
+    graph['Craiova']['Dobreta'] = 120
+    graph['Craiova']['Pitesti'] = 138
+    graph['Craiova']['RimnicuVilcea'] = 146
 
-        graph['Pitesti'] = {} 
-        graph['Pitesti']['Craiova'] = 138
-        graph['Pitesti']['Bucharest'] = 101
-        graph['Pitesti']['RimnicuVilcea'] = 97
+    graph['Pitesti'] = {} 
+    graph['Pitesti']['Craiova'] = 138
+    graph['Pitesti']['Bucharest'] = 101
+    graph['Pitesti']['RimnicuVilcea'] = 97
 
-        graph['Fagaras'] = {} 
-        graph['Fagaras']['Sibiu'] = 99
-        graph['Fagaras']['Bucharest'] = 211
+    graph['Fagaras'] = {} 
+    graph['Fagaras']['Sibiu'] = 99
+    graph['Fagaras']['Bucharest'] = 211
 
-        graph['Dobreta'] = {} 
-        graph['Dobreta']['Mehadia'] = 75
-        graph['Dobreta']['Craiova'] = 120
+    graph['Dobreta'] = {} 
+    graph['Dobreta']['Mehadia'] = 75
+    graph['Dobreta']['Craiova'] = 120
 
-        graph['Bucharest'] = {} 
-        graph['Bucharest']['Fagaras'] = 211
-        graph['Bucharest']['Pitesti'] = 101
-        graph['Bucharest']['Giurgiu'] = 90
+    graph['Bucharest'] = {} 
+    graph['Bucharest']['Fagaras'] = 211
+    graph['Bucharest']['Pitesti'] = 101
+    graph['Bucharest']['Giurgiu'] = 90
 
-        graph['Giurgiu'] = {} 
-        graph['Giurgiu']['Bucharest'] = 90
+    graph['Giurgiu'] = {} 
+    graph['Giurgiu']['Bucharest'] = 90
 
-        print("Created graph: ", graph)
+    #print("Created graph: ", graph)
 
-        return graph
+    return graph
+
+grafo_rumania = crear_grafo_rumania()
 
 grafo_jarra = {
 
-    '(0,0)': ['(4,0)', '(0,3)'],
-    '(4,0)': ['(4,3)', '(0,0)', '(1,3)'],
-    '(4,3)': ['(0,3)', '(4,0)'],
-    '(0,3)': ['(4,3)', '(0,0)', '(3,0)'],
-    '(1,3)': ['(4,3)', '(0,3)', '(1,0)', '(4,0)'],
-    '(1,0)': ['(4,0)', '(1,3)', '(0,0)', '(0,1)'],
-    '(0,1)': ['(4,1)', '(0,3)', '(0,0)', '(1,0)'],
-    '(4,1)': ['(4,3)', '(0,1)', '(4,0)', '(3,3)'],
-    '(3,0)': ['(3,3)', '(4,0)', '(0,0)', '(0,3)'],
-    '(3,3)': ['(4,3)', '(0,3)', '(3,0)', '(4,2)'],
-    '(4,2)': ['(4,3)', '(0,2)', '(4,0)', '(3,3)'],
-    '(0,2)': ['(4,2)', '(0,3)', '(0,0)', '(2,0)'],
-    '(2,0)': ['(2,3)', '(4,0)', '(0,0)', '(0,2)'],
-    '(2,3)': ['(4,3)', '(0,3)', '(2,0)', '(4,1)'],
+    '(0,0)': {'(4,0)':1, '(0,3)':1},
+    '(4,0)': {'(4,3)':1, '(0,0)':1, '(1,3)':1},
+    '(4,3)': {'(0,3)':1, '(4,0)':1},
+    '(0,3)': {'(4,3)':1, '(0,0)':1, '(3,0)':1},
+    '(1,3)': {'(4,3)':1, '(0,3)':1, '(1,0)':1, '(4,0)':1},
+    '(1,0)': {'(4,0)':1, '(1,3)':1, '(0,0)':1, '(0,1)':1},
+    '(0,1)': {'(4,1)':1, '(0,3)':1, '(0,0)':1, '(1,0)':1},
+    '(4,1)': {'(4,3)':1, '(0,1)':1, '(4,0)':1, '(3,3)':1},
+    '(3,0)': {'(3,3)':1, '(4,0)':1, '(0,0)':1, '(0,3)':1},
+    '(3,3)': {'(4,3)':1, '(0,3)':1, '(3,0)':1, '(4,2)':1},
+    '(4,2)': {'(4,3)':1, '(0,2)':1, '(4,0)':1, '(3,3)':1},
+    '(0,2)': {'(4,2)':1, '(0,3)':1, '(0,0)':1, '(2,0)':1},
+    '(2,0)': {'(2,3)':1, '(4,0)':1, '(0,0)':1, '(0,2)':1},
+    '(2,3)': {'(4,3)':1, '(0,3)':1, '(2,0)':1, '(4,1)':1},
 }
 
 grafo_granjero = {
-    '(0,0,0,0)': ['(1,1,0,0)'],
-    '(1,1,0,0)': ['(0,0,0,0)', '(0,1,0,0)'],
-    '(0,1,0,0)': ['(1,1,0,0)', '(1,1,0,1)', '(1,1,1,0)'],
-    '(1,1,0,1)': ['(0,0,0,1)', '(0,1,0,0)'],
-    '(0,0,0,1)': ['(1,1,0,1)', '(1,0,1,1)'],
-    '(1,1,1,0)': ['(0,1,0,0)', '(0,0,1,0)'],
-    '(0,0,1,0)': ['(1,1,1,0)', '(1,0,1,1)'],
-    '(1,0,1,1)': ['(0,0,0,1)', '(0,0,1,0)', '(0,0,1,1)'],
-    '(0,0,1,1)': ['(1,0,1,1)', '(1,1,1,1)'],
-    '(1,1,1,1)': ['(0,0,1,1)']
-}
-
-grafo_granjero_c = {
     '(0,0,0,0)': {'(1,1,0,0)':1},
     '(1,1,0,0)': {'(0,0,0,0)':1, '(0,1,0,0)':1},
     '(0,1,0,0)': {'(1,1,0,0)':1, '(1,1,0,1)':1, '(1,1,1,0)':1},
@@ -247,28 +268,25 @@ grafo_granjero_c = {
     '(1,1,1,1)': {'(0,0,1,1)':1}
 }
 
-graph3 = {
-    # Origen : destinos = (destino, peso_arista)
-    "S":[("A", 1), ("B", 5), ("C", 15)],
-    "A":[("G", 10), ("S", 1)],
-    "G":[("C", 5), ("A", 10)],
-    "C":[("S", 15), ("G", 5)],
-    "B":[("G", 5), ("S", 5)],
-}
-
-
 def main():
 
     tree1 = tree_searchs("Arbol 1")
-    print("Grafo granjero modificado: ", grafo_granjero_c)
 
     #print("\n\nRecorrido en profundidad: ")
     #print(next(tree1.dfs_paths(grafo_granjero, '(0,0,0,0)', '(1,1,1,1)'))) 
 
+
     #print("\n\nRecorrido en anchura: ")
     #print(next(tree1.bfs_paths(grafo_granjero, '(0,0,0,0)', '(1,1,1,1)')))
 
-    #tree1.implementacion_costo_uniforme('Arad', 'Bucharest')
+
+    #print("\n\nRecorrido costo uniforme: ")
+    #tree1.costo_uniforme(grafo_rumania, 'Arad', 'Bucharest')
+
+    
+    #print("\n\nRecorrido en profundidad con limite iterativo: ")
+    #tree1.dfs_paths_limit(grafo_jarra, '(0,0)', '(2,0)', 5)
+    
 
 if __name__ == "__main__":
     main()
